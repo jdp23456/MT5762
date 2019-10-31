8abcf4dac4b138ab04f74b29b462c31a60b49e36
James Butler <jamesccbutler@gmail.com>
2019-10-31 10:24:56 +0000

    Create Bootstrap for CI -You can input any linear model

---
+++ BootstrapCI.R
@@ -0,0 +1,34 @@
+lmBoot <- function(inputData, nBoot,regmodel){
+  
+  # Create variable myformula to use in regression model later
+  myformula<-as.formula(regmodel)
+  
+  # Create variable called input data
+  nrowdata<-nrow(inputData)
+  
+  # Create a list for compiling Bootstraps
+  bootList<-list()
+  
+  # Create a for loop to run multiple bootstraps
+  for(i in 1:nBoot){
+    
+    # Create a Sample Index
+    sampleIndex <- sample(1:nrowdata, nrowdata, replace = T)
+    
+    # Sample the dataset
+    bootData<- inputData[sampleIndex,]
+    
+    #Compile all bootstrap samples into a list called bootData
+    bootList[[i]]<-bootData
+    
+  }
+  
+  ourBootReg<- parLapply(myClust,bootList,function(itemFromList){coef(lm(myformula,data=itemFromList))})
+  
+  dataframeOfBootCoefs<-plyr::ldply(ourBootReg)
+  
+  # take central 95% of each columns esimates (simulated sampling dists) 
+  
+  parApply(myClust,dataframeOfBootCoefs,2,quantile,probs=c(0.025,0.975))
+  
+}
